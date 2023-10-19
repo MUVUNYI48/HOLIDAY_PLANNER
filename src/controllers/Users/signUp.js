8cc0d2475fa3bcb1c4ed9c1ff6e09cb6fec9090d
unknown <patrickmuvunyi@gmail.com>
2023-10-19 08:45:28 +0200

    another commit

--- src/controllers/Users/signUp.js
+++ src/controllers/Users/signUp.js
@@ -1,7 +1,7 @@
 
 import { User } from '../../models/userModels.js'
 import { createToken } from '../../utils/createToken.js';
-import { hashPassword} from '../../utils/passwordFunction.js';
+import { hashPassword } from '../../utils/passwordFunction.js';
 // import bcrypt from "jsonwebtoken"
 
 export const signUp = async (req, res) => {
@@ -11,7 +11,7 @@ export const signUp = async (req, res) => {
 
         // let newUser = User.create(req.body);
         if (user) {
-             return res.json({
+            return res.json({
                 message: "user already existed"
             })
         }
@@ -26,11 +26,12 @@ export const signUp = async (req, res) => {
             email: newUser.email
         })
 
-        console.log('new token:',token)
+        console.log('new token:', token)
 
         res.status(200).json({
             message: "user data inserted successfully",
             acccess_Token: token,
+            password: newUser.password,
             email: newUser.email,
             fullNames: newUser.fullNames,
             location: newUser.location,
@@ -40,7 +41,7 @@ export const signUp = async (req, res) => {
     } catch (error) {
         console.log(error.message);
         res.status(500).json({
-            message:"internal server error"
+            message: "internal server error"
         })
     }
 }
